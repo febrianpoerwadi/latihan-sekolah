064bdfa5e7d649d6e23f5a33ca6a0c08335fe87e
Febrian Poerwadi <55315118+febrianpoerwadi@users.noreply.github.com>
2023-05-26 15:03:55 +0700

    first commit

---
+++ Eskul/26 Mei 2023/U_if1.dpr
@@ -0,0 +1,13 @@
+program U_if1;
+
+uses
+  Forms,
+  U_if in '..\U_if.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
